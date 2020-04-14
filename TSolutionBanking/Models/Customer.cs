f6f21b80059f6b1dd298412bfc107334e787edc3
japhet98 <japhetkuntublankson1@gmail.com>
2020-04-14 00:36:59 +0000

    one

---
+++ TSolutionBanking/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSolutionBanking.Models
+{
+ public  class Customer
+    {
+        public string CustomerNo { get; set; }
+        public string AccountNumber { get; set; }
+        public string Firstname { get; set; }
+        public string  Lastname { get; set; }
+        public string Othername { get; set; }
+        public string Phone { get; set; }
+        public string AccountType { get; set; }
+        public decimal Balance { get; set; }
+        public string Username { get; set; }
+        public string  Password { get; set; }
+        public string DateRegister { get; set; }
+    }
+}
---
+++ TSolutionBanking/Models/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSolutionBanking.Models
+{
+    public class Transaction
+    {
+        public string TransactionId { get; set; }
+        public string CustomerNo { get; set; }
+        public string  TransType { get; set; }
+        public string  SenderName { get; set; }
+        public string TransDate { get; set; }
+        public decimal Amount { get; set; }
+        public string  ReceipientAccNo { get; set; }
+    }
+}
